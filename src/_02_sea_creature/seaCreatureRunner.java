ba216880cfdae567302c045a18d60fe6e67d6691
SpencerJRalph <48035530+SpencerJRalph@users.noreply.github.com>
2020-06-22 18:29:41 -0700

    awesome smurfs

---
+++ src/_02_sea_creature/seaCreatureRunner.java
@@ -0,0 +1,15 @@
+package _02_sea_creature;
+
+public class seaCreatureRunner {
+public static void main(String[] args) {
+	 SeaCreature Spongebob = new SeaCreature("spongebob");
+	 Spongebob.eat();
+	 Spongebob.laugh();
+	 SeaCreature Patrick = new SeaCreature("patrick");
+	 Patrick.eat();
+	 Patrick.laugh();
+	 SeaCreature Squidward = new SeaCreature("squidward");
+	 Squidward.eat();
+	 Squidward.laugh();
+}
+}
---
+++ src/_03_smurf/SmurfRunner.java
@@ -0,0 +1,9 @@
+package _03_smurf;
+
+public class SmurfRunner {
+public static void main(String[] args) {
+	Smurf hi = new Smurf("Handy");
+	hi.eat();
+	System.out.println(hi.getName());
+}
+}
